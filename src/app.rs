d2d989a1407730b1447e7324a12672ed60566028
maliByatzes <malib2027@gmail.com>
2024-11-12 15:18:25 +0200

    initialize app and main fn

---
+++ src/app.rs
@@ -0,0 +1,41 @@
+pub enum CurrentScreen {
+    Main,
+    Menu,
+    Commands,
+    Exiting,
+}
+
+pub enum CurrentlyPlaying {
+    Block(Block),
+}
+
+pub enum Level {
+    Easy,
+    Medium,
+    Hard,
+    Evil,
+}
+
+pub struct Block {
+    value: Option<u8>,
+    row: u8,
+    col: u8,
+}
+
+pub struct App {
+    pub game_board: Vec<Block>, // NOTE: Could make this a fixed sized array
+    pub level: Level,
+    pub current_screen: CurrentScreen,
+    pub currently_playing: Option<CurrentlyPlaying>,
+}
+
+impl App {
+    pub fn new() -> App {
+        App {
+            game_board: Vec::<Block>::with_capacity(81),
+            level: Level::Easy,
+            current_screen: CurrentScreen::Main,
+            currently_playing: None,
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+use std::{error::Error, io};
+
+use app::App;
+use crossterm::{
+    event::{DisableMouseCapture, EnableMouseCapture},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use ratatui::{
+    prelude::{Backend, CrosstermBackend},
+    Terminal,
+};
+
+mod app;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // setup terminal
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    let mut app = App::new();
+    run_app(&mut terminal, &mut app);
+
+    // restore terminal
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+
+    Ok(())
 }
+
+fn run_app<B: Backend>(terminal: &mut Terminal<B>, app: &mut App) {}
